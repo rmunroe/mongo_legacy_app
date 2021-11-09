445e888367b8e022feb05769cbb06d2c091e45e1
jscanzoni <jason@scnz.co>
2021-11-09 14:29:19 -0500

    update to connection string

--- test.php
+++ test.php
@@ -9,12 +9,13 @@ $app_id= getenv('APP');
 $client = new MongoDB\Client(
     'mongodb+srv://'.$creds.'@'.$url);
 
-$db = $client->selectDB('legacy_apps');
+//$db = $client->selectDB('legacy_apps');
 
-$config_collection = new MongoCollection($db, 'app_config');
+$config_collection = $client->legacy_apps->app_config;
+/*= new MongoCollection($db, 'app_config');
 $config_collection = new MongoCollection($db, 'data');
 $config_collection = new MongoCollection($db, 'default_data');
-
+*/
 $app_query = array('app_id' => $app_id);
 
 $config = $config_collection->find($app_query);
