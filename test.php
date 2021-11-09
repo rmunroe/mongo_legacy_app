d6a2ed2996cc290fc34c342ba92a70a8430dbc3d
jscanzoni <jason@scnz.co>
2021-11-09 14:39:15 -0500

    config

--- test.php
+++ test.php
@@ -9,16 +9,14 @@ $app_id= getenv('APP');
 $client = new MongoDB\Client(
     'mongodb+srv://'.$creds.'@'.$url);
 
-//$db = $client->selectDB('legacy_apps');
 
 $config_collection = $client->legacy_apps->app_config;
-/*= new MongoCollection($db, 'app_config');
-$config_collection = new MongoCollection($db, 'data');
-$config_collection = new MongoCollection($db, 'default_data');
-*/
-$app_query = array('app_id' => $app_id);
+$config_collection = $client->legacy_apps->data;
+$config_collection = $client->legacy_apps->default_data;
 
-$config = $config_collection->find($app_query);
+$app_query = ['app_id' => $app_id];
+
+$config = $config_collection;
 
 var_dump($config);
 
