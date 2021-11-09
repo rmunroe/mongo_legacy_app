c1887960d117b49ce2a5b22411ace1b6e3b6dc95
jscanzoni <jason@scnz.co>
2021-11-09 14:26:36 -0500

    added test query

--- test.php
+++ test.php
@@ -9,8 +9,20 @@ $app_id= getenv('APP');
 $client = new MongoDB\Client(
     'mongodb+srv://'.$creds.'@'.$url);
 
-$db = $client->test;
+$db = $client->selectDB('legacy_apps');
 
-echo $app_id;
+$config_collection = new MongoCollection($db, 'app_config');
+$config_collection = new MongoCollection($db, 'data');
+$config_collection = new MongoCollection($db, 'default_data');
+
+$app_query = array('app_id' => $app_id);
+
+$config = $config_collection->find($app_query);
+
+var_dump($config->data);
+
+
+
+//echo $app_id;
 
 ?>
